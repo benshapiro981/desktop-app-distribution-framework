efaa674999be6bf3f6716f20e6964cba8c6fc44b
benshapiro981 <shapiro.benjamin.d@gmail.com>
2025-01-19 15:30:38 -0800

    implement functionality to determine subscription status from python app

--- desktop-app/basic_app.py
+++ desktop-app/basic_app.py
@@ -3,15 +3,23 @@ from __future__ import annotations
 import sys 
 import os
 import time
+import warnings
 
+import cowsay
 import dotenv
 import requests
+from google.oauth2.credentials import Credentials as OAuth2Credentials
+from google.cloud.firestore import Client as FirestoreClient
+
+# TODO: comment this next line and get rid of the deprecation warning instead of suppressing it 
+warnings.simplefilter("ignore", UserWarning)
 
 # get the firebase web api key 
 dotenv_path = os.path.join(os.getcwd(), ".env")
-assert dotenv.load_dotenv(dotenv_path) or os.environ.get("FBASE_API_KEY"), "unable to read in firebase api key"
+assert dotenv.load_dotenv(dotenv_path) or (os.environ.get("FBASE_API_KEY") and os.environ.get("FBASE_PROJECT_ID")), "unable to read in firebase api key"
 
 FBASE_API_KEY = os.environ["FBASE_API_KEY"]
+FBASE_PROJECT_ID = os.environ["FBASE_PROJECT_ID"]
 
 def get_user_data(access_tok: str) -> dict | None:
     resp = requests.post(
@@ -137,13 +145,65 @@ def login_loop() -> tuple[str, str]:
             print("invalid credentials supplied, please try again")
     except KeyboardInterrupt:  # don't want to print the exception stack 
         raise SystemExit(0)
+    
+import firebase_admin
+def is_subscription_active(
+    access_tok: str, 
+    refresh_tok: str, 
+    user_id: str,
+) -> bool:
+    # make google oauth2 token
+    creds = OAuth2Credentials(
+        access_tok, 
+        refresh_tok,
+    )
+    
+    db = FirestoreClient(
+        project=FBASE_PROJECT_ID,
+        credentials=creds,
+    )
+    
+    results = db.collection("customers", user_id, "subscriptions").where(
+        "status", "==", "active" 
+    ).stream()
+    
+    stat = False
+    
+    for doc in results:
+        if doc.to_dict()["status"] == "active":
+            # NOTE: in our implementation there 
+            stat = True
+    
+    return stat
         
+
+def get_cla_parser():
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "-f", "--force-relogin",
+        help="force relogin at the beginning of the application",
+        action="store_true"
+    )
+    
+    return parser
+
 
 def main():
     # TODO: refactor the if statements
+    cla_parser = get_cla_parser()
+    
+    args = cla_parser.parse_args()
+    
+    
     
     access_tok = get_acc_tok()  # either str or None
     refresh_tok = get_refresh_tok()  # either str or None
+    
+    if args.force_relogin:
+        # force case 3
+        access_tok = None
+        refresh_tok = None
     
     user_data: dict | None = None
     if access_tok:
@@ -191,10 +251,22 @@ def main():
         
     assert user_data and isinstance(user_data, dict)
     
-    print(f"hello, {user_data["email"]}, your userid is {user_data["localId"]}. starting a long running task now....")
-    time.sleep(10)
-    print("task completed!")
+    print(f"hello, {user_data["email"]}, your userid is {user_data["localId"]}. Checking your subscription status....")
     
+    
+    sub_active = is_subscription_active(access_tok, refresh_tok, user_data["localId"])
+    
+    if sub_active:
+        print("found an active subscription, accessig premium content...")
+        print()
+        cowsay.cow("mom deleted roblox uwu")
+        print()
+        
+    else:
+        print("active subscription not found, please go on your online profile and activate your premium account")
+
+    time.sleep(2)
+
     logout_inp = input("would you like to log out? (y for yes) ")
     
     if logout_inp.lower().strip().startswith("y"):  # Y, y, yes, ye, YES, ...
